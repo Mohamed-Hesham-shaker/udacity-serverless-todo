ad190dd2bab9caf2d8e44d3377d7552677d838bf
Mohamed Hesham <mohamed.hesham@thirdwayv.com>
2021-08-15 15:50:21 +0200

    Import logger

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-
+import { createLogger } from '../../utils/logger'
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
 
